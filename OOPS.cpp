2261054fa987584da1004eeb182b4623b7319007
Abhishek <abhishekchauhan85805@gmail.com>
2022-09-19 15:51:53 +0530

    OOPS.cpp modified

--- OOPS.cpp
+++ OOPS.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 };*/
 
+
 // Class for data hiding 
 /*class rectangle{
     private:
@@ -34,6 +35,7 @@ using namespace std;
         }
     
 };*/
+
 
 // Class for Constructor
 /*class rectangle{
@@ -63,11 +65,75 @@ using namespace std;
         }
 };*/
 
+
+// Class for Scope Resolution
+/*class rectangle{
+	private:
+		float length;
+		float bredth;
+	public:
+		rectangle(float l=1, float b=1){ // By doing this we don't need to write default constructor separately.
+			length = l;
+			bredth = b;
+		}
+		float area(){ // If we create functions inside the class then they are known as inline function.Inline 
+        // function is a function that is expanded in line when it is called.
+			return length*bredth;
+		}
+		float perimeter(); // Functions define outside the class are not known as outline function, because 
+		//during the call it will go to the function and after the function it will come back to the main 
+		// function. So it is good practice in c++ to write code outside the class.  
+};
+float rectangle::perimeter(){ // Scope resolution shows that the scope of perimeter function is in the class rectangle.
+	return 2*(length+bredth);
+}*/
+
+
+// Class for this pointer
+/*class Rectangle{
+    private:
+        int length;
+        int breadth;
+    public:
+        Rectangle(int length=1, int breadth=2){
+            this->length = length; // Name of local variable is same as the member variable, so
+            // we use this pointer to differentiate. 
+            this->breadth = breadth;
+        }
+        int area(){
+            return length*breadth;
+        }
+};*/
+
+
+// Add two complex number
+/*class Complex{
+    private:
+        int a;
+        int b;
+    public:
+        Complex(int a=0, int b=0){
+            this->a = a;
+            this->b = b;
+        }
+        Complex add(Complex c){
+            Complex temp;
+            temp.a = a+c.a;
+            temp.b = b+c.b;
+            return temp;
+        }
+        void display(){
+            cout<<a<<" + "<<b<<"i";
+        }
+};*/
+
+
 int main(){
     // Dot Operator: Used for direct member selection via object name. 
     /*rectangle r;
     cin>>r.length>>r.breadth;
     cout<<"Area: "<<r.area()<<endl;*/
+
 
     //  Arrow Operator: gives the value held by variable_name to class variable pointer_name.
     /*rectangle *r, *r1, r2;
@@ -80,17 +146,35 @@ int main(){
     cin>>r1->length>>r1->breadth;
     cout<<"Area: "<<r1->area()<<endl;*/
 
+
     // Data Hiding
     /*rectangle r;
     r.setLength(10);
     r.setBreadth(20);
     cout<<"Area: "<<r.area()<<endl;*/
 
+
     // Constructor
     /*rectangle r, r1(10, 20), r2(r1);
     cout<<r.area()<<" and "<<r1.area()<<" and "<<r2.area()<<endl;*/
 
+
+    // Scope Resolution
+    /*rectangle r1;
+	rectangle r2(10,20);
+	cout<<"Area: "<<r2.area()<<endl;
+	cout<<"Perimeter: "<<r2.perimeter();*/
+
+
+    // this pointer
+    /*Rectangle r;
+    cout<<r.area()<<endl;*/
     
+
+    // Add two complex number
+    /*Complex c(4, 5), c1(5, 6), c2;
+    c2 = c.add(c1);
+    c2.display();*/
 
     return 0;
 }
